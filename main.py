b6efba3bf8731834b84513e4e506fc29e62dfbbe
CedricBorko <cedric@borkoland.de>
2021-03-13 11:55:20 +0100

    Display of output can be vertical or horizontal

--- main.py
+++ main.py
@@ -8,20 +8,38 @@ from token import OPERATORS
 print(OPERATORS)
 
 
-def calculate_result(res, literals):
+def calculate_result(res, literals, vertical=False):
     combinations = list(itertools.product([1, 0], repeat=len(literals)))
     truth_table = []
-    for i in range(len(combinations) - 1, -1, -1):
-        translator = {}
-        for key_ in literals:
-            translator[key_] = combinations[i][literals.index(key_)]
 
-        interpreter = Interpreter(translator)
-        truth_table.append([int(interpreter.read(res)), [translator[literal] for literal in literals]])
+    if vertical:
+        for i in range(len(combinations) - 1, -1, -1):
+            translator = {}
+            for key_ in literals:
+                translator[key_] = combinations[i][literals.index(key_)]
 
-    print(' '.join(literals), "S")
-    for entry in truth_table:
-        print(' '.join(map(str, entry[1])), entry[0])
+            interpreter = Interpreter(translator)
+            truth_table.append([int(interpreter.read(res)), [translator[literal] for literal in literals]])
+
+        print(' '.join(literals), "S")
+        for entry in truth_table:
+            print(' '.join(map(str, entry[1])), entry[0])
+
+    else:
+        for i in range(len(combinations) - 1, -1, -1):
+            translator = {}
+            for key_ in literals:
+                translator[key_] = combinations[i][literals.index(key_)]
+            interpreter = Interpreter(translator)
+            truth_table.append(int(interpreter.read(res)))
+
+        r = [literals[i] + " " + ' '.join(map(str, [c[i] for c in reversed(combinations)]))
+                                                         for i in range(len(literals))]
+
+        for e in r:
+            print(e)
+
+        print("f" + " " + ' '.join(map(str, truth_table)))
 
 
 while True:
@@ -30,6 +48,4 @@ while True:
     tokens = lexer.generate_tokens()
     parser = Parser(tokens)
     result = parser.parse()
-    calculate_result(result, sorted(parser.literals))
-
-
+    calculate_result(result, sorted(parser.literals), True)
